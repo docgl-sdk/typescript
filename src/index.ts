68f47ce2e9a1a9096bad1fbbf4a8028555517043
Gabriel <gabriel.longhini@getstic.ky>
2026-05-31 12:51:34 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export { DocglClient, DocglError } from "./client";
+export type {
+  GenerateRequest,
+  GenerateResponse,
+  JobStatusResponse,
+  JobStatus,
+  DocumentInput,
+  DocumentVariables,
+  VariableValue,
+  TableRow,
+  QueuedJob,
+  ValidationError,
+} from "./types";
